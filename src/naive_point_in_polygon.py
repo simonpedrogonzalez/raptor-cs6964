9845abb7e4f23c4ef35f5f08964a99f8e3f008fa
Simón Pedro <simon.pedro.g@gmail.com>
2025-01-30 10:11:24 -0700

    add the other shapefiles and write down runtimes

--- src/naive_point_in_polygon.py
+++ src/naive_point_in_polygon.py
@@ -5,7 +5,7 @@ from rasterstats import zonal_stats
 from geocube.vector import vectorize # couldnt make it work yet
 import rioxarray as rxr
 import rasterio as rio
-from shapely.geometry import shape, Point, MultiPoint
+from shapely.geometry import shape, Point, MultiPoint, box
 from rasterio.features import shapes
 from geopandas import GeoDataFrame
 import matplotlib.pyplot as plt
@@ -81,7 +81,7 @@ def xy_np(transform, cols, rows, offset='center'):
     locs = _transnp @ _translt @ pts
     return locs[0], locs[1]
 
-def naive_point_in_polygon(raster, polygon):
+def zonal_mean(raster, polygon, method="naive"):
     # If the TODOs are solved, this should be equivalent
     # to the Naive PIP method shown in the paper.
     # Naive Point-in-Polygon
@@ -93,19 +93,29 @@ def naive_point_in_polygon(raster, polygon):
     transform = raster.transform
     crs = raster.crs
 
-    points = []
+    # It's not clear if I should take the height and width of the raster or
+    # the bounding box of the polygon to create the meshgrid
     n_cols = raster.width
     n_rows = raster.height
+    
     row_indices, col_indices = np.meshgrid(np.arange(n_rows), np.arange(n_cols), indexing='ij')
     row_indices = row_indices.ravel()
     col_indices = col_indices.ravel()
 
     x, y = xy_np(transform, row_indices, col_indices)
-    xy_gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(x, y), crs=crs)
+    points = gpd.GeoDataFrame(geometry=gpd.points_from_xy(x, y), crs=crs)
+
+    polygon = gpd.GeoDataFrame(geometry=[polygon], crs=crs)
+
     # get points in polygon and create a 2d mask to apply to the raster
-    # TODO: the polygon.contains is super slow, it is actually the only slow part of the algo
-    # we could try another library like xarray or geocube
-    mask = polygon.contains(xy_gdf).values.ravel().reshape((n_rows, n_cols))
+    if method == "naive":
+        indices = naive_point_in_polygon(points, polygon)
+    elif method == "qsplit":
+        indices = qsplit_point_in_polygon(points, polygon, 0, 3)
+    
+    mask = np.zeros((n_rows, n_cols), dtype=bool)
+    mask[row_indices[indices], col_indices[indices]] = True
+
     # here is the problem, I should  be able to read the non masked values,
     # but it seems I can only read the whole raster and then mask it
     # TODO: test other reading libraries that allows reading non masked values
@@ -113,19 +123,68 @@ def naive_point_in_polygon(raster, polygon):
     mean = data[mask].mean()
     return mean
 
-def qtree_point_in_polygon(raster, polygon):
+def naive_point_in_polygon(points: GeoDataFrame, polygon: GeoDataFrame):
+    indices = gpd.tools.sjoin(points, polygon, predicate='within', how='inner').index
+    return indices
+
+def qsplit_point_in_polygon(points: GeoDataFrame, polygon: GeoDataFrame, depth: int, max_depth: int, crs=None):
     # https://stackoverflow.com/questions/36282306/is-there-way-to-optimize-speed-of-shapely-geometry-shape-containsa-point-call
     # https://gis.stackexchange.com/questions/23688/splitting-vector-into-equal-smaller-parts-using-qgis/23694#23694
     # https://gis.stackexchange.com/questions/120955/understanding-use-of-spatial-indexes-with-rtree/144764#144764
-    return None
+    # It would be same as above but in the PIP phase, it would
+    # apply a quadtree to the polygon and then check the points
+
+    if depth >= max_depth or len(points) <= 100:  # We might need to adjust this threshold
+        polygon = gpd.GeoDataFrame(geometry=polygon, crs=crs)
+        return gpd.tools.sjoin(points, polygon, predicate='within', how='inner').index
+
+    minx, miny, maxx, maxy = polygon.total_bounds
+    cx, cy = (minx + maxx) / 2, (miny + maxy) / 2
+
+    quadrants = [
+        box(minx, cy, cx, maxy),
+        box(cx, cy, maxx, maxy),
+        box(minx, miny, cx, cy),
+        box(cx, miny, maxx, cy) 
+    ]
+
+    # print(f"Depth: {depth}: boxes: {len(quadrants)}")
+    
+    indices = []
+    for quad in quadrants:
+        sub_poly = polygon.intersection(quad).dropna()
+        if not sub_poly[0].is_empty:
+            sub_points = points[points.geometry.within(quad)]
+            indices.extend(qsplit_point_in_polygon(sub_points, sub_poly, depth + 1, max_depth, polygon.crs))
+
+    return indices
 
 first_element = vector_layer.iloc[0]
 polygon = first_element.geometry
+method = "naive"
+
 t0 = t.time()
-mean = naive_point_in_polygon(raster_layer, polygon)
+mean = zonal_mean(raster_layer, polygon, method)
 t1 = t.time()
 print(f"Naive time: {t1-t0}")
 print(f"Naive value: {mean}")
 assert np.isclose(true_mean, mean, atol=1e-6)
+
+method = "qsplit"
+t0 = t.time()
+mean = zonal_mean(raster_layer, polygon, method)
+t1 = t.time()
+print(f"Qsplit time: {t1-t0}")
+print(f"Qsplit value: {mean}")
+assert np.isclose(true_mean, mean, atol=1e-6)
+
 print(f"Element: {first_element['SOVEREIGNT']}")
 print('done')
+
+# Times:
+# Rasterstats time: 5.2760491371154785 (But it calculates all the stats for all polys)
+# True value: 301.062958996328
+# Naive time: 0.5756285190582275
+# Naive value: 301.0629577636719
+# Qsplit time: 1.034510850906372 # Maybe it's useful for higher resolution rasters
+# Qsplit value: 301.0629577636719
